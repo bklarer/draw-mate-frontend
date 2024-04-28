f46b5f677a6efe4ebcbfe0a117b96c6217bd0bbf
Benjamin <benjaminklarer@gmail.com>
2024-04-27 21:49:19 -0700

    Add button to participants

--- app/participants/page.tsx
+++ app/participants/page.tsx
@@ -1,18 +1,21 @@
 import ParticipantCard from "@/components/ParticipantCard";
+import { Button } from "@/components/ui/button";
 
 export default function ParticipantPage() {
   //TODO: I'll just do a modal for a single participant to edit/delete
 
   return (
-    <div>
-      <h1>Participant Page</h1>
-      <div className="w-full">
-        <h1 className="text-4xl text-center">Event Page</h1>
-        <div className="grid grid-cols-4 gap-10 mt-10">
-          {[1, 2, 3, 4, 5, 6].map((event) => (
-            <ParticipantCard key={event} className="w-full" />
-          ))}
-        </div>
+    <div className="w-full mt-10">
+      <div className="grid grid-cols-3">
+        <h1 className="text-4xl text-center col-start-2 place-self-center">
+          Participant Page
+        </h1>
+        <Button className="col-start-3 place-self-end">Add Participant</Button>
+      </div>
+      <div className="grid grid-cols-4 gap-10 mt-10">
+        {[1, 2, 3, 4, 5, 6].map((event) => (
+          <ParticipantCard key={event} className="w-full" />
+        ))}
       </div>
     </div>
   );
